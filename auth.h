37001a1d424eb23bb3aaaceee23dbf1fa9dbc222
gn027c <tanloi.mighty@gmail.com>
2026-04-21 22:53:09 +0700

    [Loi/21-04] Fix magic numbers in event and auth modules

--- auth.h
+++ auth.h
@@ -1,14 +1,20 @@
 #ifndef AUTH_H
 #define AUTH_H
 
+#include "user.h"
+
+#define MAX_LOGIN_ATTEMPTS 3
+
 typedef struct
 {
-    char studentId[20];
-    char password[255];
-    int role;
+    char studentId[ID_LENGTH];
+    char password[PASSWORD_LENGTH];
+    int role; // 0=Member, 1=BCN
     int isLocked;
     int failCount;
 } Account;
+
+
 
 void updateAccount(Account *updated);
 int findAccoutById(char *id, Account *result);
