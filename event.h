30411708ed423fd25b4be795335617f687090e5e
gn027c <tanloi.mighty@gmail.com>
2026-04-21 22:53:09 +0700

    [Loi/21-04] Fix magic numbers in event and auth modules

--- event.h
+++ event.h
@@ -1,24 +1,40 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include "user.h"
+
+#define MAX_STAFF_PER_EVENT 30
+#define EVENT_ID_LENGTH 10
+#define DESC_LENGTH 255
+#define LOCATION_LENGTH 255
+#define DATE_LENGTH 11
+
+typedef enum
+{
+    STATUS_UPCOMING = 0,
+    STATUS_ONGOING = 1,
+    STATUS_FINISHED = 2
+} EventStatus;
+
 typedef struct
 {
-    char studentId[20];
+    char studentId[ID_LENGTH];
     int role;
-    char description[255];
+    char description[DESC_LENGTH];
 } StaffEntry;
 
 typedef struct
 {
-    char eventId[9];
-    char name[255];
-    char description[255];
-    char location[255];
-    char startDate[11];
-    char endDate[11];
-    int status;
+    char eventId[EVENT_ID_LENGTH];
+    char name[NAME_LENGTH];
+    char description[DESC_LENGTH];
+    char location[LOCATION_LENGTH];
+    char startDate[DATE_LENGTH];
+    char endDate[DATE_LENGTH];
+    int status; // Will use EventStatus values
     int staffCount;
-    StaffEntry staffList[30];
+    StaffEntry staffList[MAX_STAFF_PER_EVENT];
 } Event;
+
 
 #endif
